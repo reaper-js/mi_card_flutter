5cea4e7ebc515b47a5308535f0a73fe1793ac9a2
dramati <jatinsaini191@gmail.com>
2021-06-25 08:37:52 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(CardApp());
+}
+
+class CardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80.0,
+                backgroundImage: AssetImage('images/yo.jpg'),
+              ),
+              Text(
+                "Jatin Saini",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                ),
+              ),
+              Text("Software Developer",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.white,
+                  ))
+            ]),
       ),
-    ),
-  );
+    ));
+  }
 }
